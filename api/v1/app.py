ae0b96d4838e901af41b78f2e781fa49f712d84e
theAstralProgrammer0 <tdaisioso@gmail.com>
2024-05-28 22:13:26 +0100

    task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,39 +4,37 @@ from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
-from flasgger import Swagger
 
 
+# instantiate Flask app instance
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(app_views, url_prefix='/api/v1')
+# register 'app_views' blueprint to app
+app.register_blueprint(app_views)
+# instantiate CORS instance to share resources from app
+# to other web apps hosted on external domain
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown(obj):
-    """method to teardown to end the db session"""
+    """teardown meth to end the db session"""
     from models import storage
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """ Loads a custom 404 page not found """
+def not_found(error):
+    """returns a response with 404 status code and json obj data"""
     return make_response(jsonify({"error": "Not found"}), 404)
 
-
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone - RESTful API',
-    'description': 'This api was built for the ALX restful api project.\
-    All necessary documentations will be shown below',
-    'uiversion': 3}
-
-Swagger(app)
+@app.errorhandler(500)
+def server_err(error):
+    """returns a response with 500 status code and json obj data"""
+    return make_response(jsonify({"error": "Internal Server Error"}), 500)
 
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = int(getenv('HBNB_API_PORT', default='5000'))
 
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, debug=True, threaded=True)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,86 +3,78 @@
 This file contains the Review module
 """
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
+from flask import jsonify, abort, request as req, make_response as mr
 from models import storage
 from models.place import Place
 from models.review import Review
 from models.user import User
-from flasgger.utils import swag_from
+from models.state import State
 
 
-@app_views.route('/places/<string:place_id>/reviews',
-                 methods=['GET'], strict_slashes=False)
-@swag_from('documentation/reviews/get.yml', methods=['GET'])
-def get_all_reviews(place_id):
-    """ get reviews from a spcific place """
-    place = storage.get(Place, place_id)
-    if place is None:
+# GET '/api/v1/states' RESTful API endpoint
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """get the states from storage"""
+    states = storage.all(State).values()
+    # jsonify list of dicts of state objs and return
+    return mr(jsonify([state.to_dict() for state in states]), 200)
+
+# GET '/api/v1/states/<state_id>' RESTful API endpoint 
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state_by_id(state_id):
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    reviews = [obj.to_dict() for obj in place.reviews]
-    return jsonify(reviews)
+    return mr(jsonify(state.to_dict()), 200)
 
-
-@app_views.route('/reviews/<string:review_id>', methods=['GET'],
-                 strict_slashes=False)
-@swag_from('documentation/reviews/get_id.yml', methods=['GET'])
-def get_review(review_id):
-    """ get review by id"""
-    review = storage.get(Review, review_id)
-    if review is None:
+# DELETE '/api/v1/states/<state_id>' RESTful API endpoint
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state_by_id(state_id):
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    return jsonify(review.to_dict())
-
-
-@app_views.route('/reviews/<string:review_id>', methods=['DELETE'],
-                 strict_slashes=False)
-@swag_from('documentation/reviews/delete.yml', methods=['DELETE'])
-def del_review(review_id):
-    """ delete review by id"""
-    review = storage.get(Review, review_id)
-    if review is None:
-        abort(404)
-    review.delete()
+    storage.delete(state)
     storage.save()
-    return jsonify({})
+    return mr(jsonify({}), 200)
 
+# POST '/api/v1/states' RESTful API endpoint
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    if req.json:
+        if 'name' in req.json:
+            data = req.get_json()
+            new_state = State(**data)
+            storage.new(new_state)
+            storage.save()
+            return mr(jsonify(new_state.to_dict()), 201)
+        elif 'states' in req.json:
+            data = req.get_json()
+            new_states = data.get('states', [])
+            for state in new_states:
+                new_state = State(**state)
+                storage.new(new_state)
+                storage.save()
+            return mr(jsonify([s for s in new_states]), 201)
+        else:
+            abort(400, description='Missing name')
+    abort(400, description='Not a JSON')
 
-@app_views.route('/places/<string:place_id>/reviews', methods=['POST'],
-                 strict_slashes=False)
-@swag_from('documentation/reviews/post.yml', methods=['POST'])
-def create_obj_review(place_id):
-    """ create new instance """
-    place = storage.get(Place, place_id)
-    if place is None:
+# PUT '/api/v1/states/<state_id>' RESTful API endpoint
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state_by_id(state_id):
+    ignore = ['id', 'created_at', 'updated_at']
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    if 'user_id' not in request.get_json():
-        return make_response(jsonify({"error": "Missing user_id"}), 400)
-    if 'text' not in request.get_json():
-        return make_response(jsonify({"error": "Missing text"}), 400)
-    kwargs = request.get_json()
-    kwargs['place_id'] = place_id
-    user = storage.get(User, kwargs['user_id'])
-    if user is None:
-        abort(404)
-    obj = Review(**kwargs)
-    obj.save()
-    return (jsonify(obj.to_dict()), 201)
-
-
-@app_views.route('/reviews/<string:review_id>', methods=['PUT'],
-                 strict_slashes=False)
-@swag_from('documentation/reviews/put.yml', methods=['PUT'])
-def post_review(review_id):
-    """ updates by id """
-    if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    obj = storage.get(Review, review_id)
-    if obj is None:
-        abort(404)
-    for key, value in request.get_json().items():
-        if key not in ['id', 'user_id', 'place_id', 'created_at', 'updated']:
-            setattr(obj, key, value)
-    storage.save()
-    return jsonify(obj.to_dict())
+    if req.json and isinstance(req.json, dict):
+        data = req.get_json()
+        for key, value in data.items():
+            if key in ignore:
+                pass
+            else:
+                state = storage.get(State, state_id)
+                setattr(state, key, value)
+                storage.save()
+            return mr(jsonify(state.to_dict()), 200)
+    else:
+        abort(400, description="Not a JSON")
